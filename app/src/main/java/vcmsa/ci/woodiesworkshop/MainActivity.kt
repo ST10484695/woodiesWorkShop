4c6896e5f8fca570817ae50eb9a26a160c157665
Shailynn Devereux <ST10484695@vcconnect.edu.za>
2025-04-17 08:26:40 +0200

    added images and color

--- app/src/main/java/vcmsa/ci/woodiesworkshop/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/woodiesworkshop/MainActivity.kt
@@ -8,13 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit private var btnStart: Button
+    private lateinit var btnStart: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnStart=findViewById(R.id.btnStart)
 
         btnStart.setOnClickListener{
             val intent = Intent(this,MainActivity2::class.java)
